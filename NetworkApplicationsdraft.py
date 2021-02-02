92217ab6b69f7410afbc4bd71999fdae4ee0e29d
Whitehead <whiteh13@vdi-scc203-28.lancs.local>
2021-02-02 23:11:51 +0000

    Working version added, draft kept for reference

--- NetworkApplicationsdraft.py
+++ NetworkApplicationsdraft.py
@@ -21,34 +21,32 @@ def setupArgumentParser() -> argparse.Namespace:
         subparsers = parser.add_subparsers(help='sub-command help')
         
         parser_p = subparsers.add_parser('ping', aliases=['p'], help='run ping')
-        parser_p.set_defaults(timeout=4)
         parser_p.add_argument('hostname', type=str, help='host to ping towards')
-        parser_p.add_argument('--count', '-c', nargs='?', type=int,
+        parser_p.add_argument('count', nargs='?', type=int,
                               help='number of times to ping the host before stopping')
-        parser_p.add_argument('--timeout', '-t', nargs='?',
+        parser_p.add_argument('timeout', nargs='?',
                               type=int,
                               help='maximum timeout before considering request lost')
         parser_p.set_defaults(func=ICMPPing)
 
         parser_t = subparsers.add_parser('traceroute', aliases=['t'],
                                          help='run traceroute')
-        parser_t.set_defaults(timeout=4, protocol='icmp')
         parser_t.add_argument('hostname', type=str, help='host to traceroute towards')
-        parser_t.add_argument('--timeout', '-t', nargs='?', type=int,
+        parser_t.add_argument('timeout', nargs='?', type=int,
                               help='maximum timeout before considering request lost')
-        parser_t.add_argument('--protocol', '-p', nargs='?', type=str,
+        parser_t.add_argument('protocol', nargs='?', type=str,
                               help='protocol to send request with (UDP/ICMP)')
         parser_t.set_defaults(func=Traceroute)
 
         parser_w = subparsers.add_parser('web', aliases=['w'], help='run web server')
         parser_w.set_defaults(port=8080)
-        parser_w.add_argument('--port', '-p', type=int, nargs='?',
+        parser_w.add_argument('port', type=int, nargs='?',
                               help='port number to start web server listening on')
         parser_w.set_defaults(func=WebServer)
 
         parser_x = subparsers.add_parser('proxy', aliases=['x'], help='run proxy')
         parser_x.set_defaults(port=8000)
-        parser_x.add_argument('--port', '-p', type=int, nargs='?',
+        parser_x.add_argument('port', type=int, nargs='?',
                               help='port number to start web server listening on')
         parser_x.set_defaults(func=Proxy)
 
@@ -97,97 +95,106 @@ class NetworkApplication:
 
 class ICMPPing(NetworkApplication):
 
-    def buildHeader(self, checksum=0):
-        # header is type (8), code (8), checksum (16), id (16), sequence_num (16)
-        hdr = struct.pack("!BBHHH",
-                          ICMP_ECHO_REQUEST,
-                          0,
-                          checksum,
-                          self.own_id,
-                          self.seq_number)
+    def buildHeader(self, icmpSocket, destinationAddress, sequence_num=1, checksum=0):
+        ID = sequence_num
+
+        hdr = struct.pack('!BBHHH', ICMP_ECHO_REQUEST, 0, checksum, sequence_num, ID)
+        #print("Construced size = " + str(len(hdr)))
+
+        """
+        hdr = struct.pack('!4s4sIIII',
+                          sourceAddress.encode('ascii'), # source address
+                          destinationAddress.encode('ascii'), # destination address
+                          ICMP_CODE, # protocol id
+                          checksum, # checksum, initially 0 before calculation
+                          ID, # ID of header
+                          1 # sequence number
+                          )"""
+
+        # Header is sourceaddress, destaddress, protocol, checksum, id, and sequence number
+        #hdr2 = struct.pack('!4s4sbHHh', sourceAddress.encode('ascii'), destinationAddress.encode('ascii'), ICMP_CODE, checksum, ID, 1)
+        
         return hdr
+
+    def check_valid_ip4_addr(self, addr):
+        segments = addr.split(".")
+
+        if not len(segments) == 4:
+            return False
+        for segment in segments:
+            try:
+                num = int(segment)
+            except ValueError:
+                return False
+            if num > 255:
+                return False
+        return True
 
     def receiveOnePing(self, icmpSocket, destinationAddress, ID, timeout):
         # 1. Wait for the socket to receive a reply
-        icmpSocket.settimeout(timeout)
-
-        try:
-            reply, addr = icmpSocket.recvfrom(ICMP_MAX_RECV_SIZE)
-        except socket.timeout as msg:
-            print("No data received from socket within timeout period. Message: " + str(msg))
-            sys.exit(1)
-
+        reply, addr = icmpSocket.recvfrom(ICMP_MAX_RECV_SIZE)
+        print(reply)
+        
         # 2. Once received, record time of receipt, otherwise, handle a timeout
         recv_time = time.time()
         
+        # 3. Compare the time of receipt to time of sending, producing the total network delay
+        
         # 4. Unpack the packet header for useful information, including the ID
-        #print(len(reply))
-        replyHeader = struct.unpack("!BBHHH", reply[20:28]) # icmp header of the received packet, bottom of packet because of network byte-order
-        replyBody = struct.unpack("!BBHHHBBHII", reply[:20]) # body of the received packet
-
-        #print(replyHeader)
-        #print(replyBody)
+        pack = struct.unpack('!BBHHH', reply)
+        """
+        pack = reply.decode('utf-8')
+        """
         
         # 5. Check that the ID matches between the request and reply
-        if replyHeader[3] != self.own_id:
-                print("Received packet ID does not match sent packet with sequence number " + self.seq_number)
-                print("Ending program")
-                sys.exit(1)
-                
-        # 6. Return recv time + packet size
-        return (recv_time, len(reply), replyBody[5])
+        
+        # 6. Return received time and packet size
+        return recv_time, len(reply)
 
     def sendOnePing(self, icmpSocket, destinationAddress, ID):
         # 1. Build ICMP header
-
-        # dummy header
-        packet = self.buildHeader()
+        pack = self.buildHeader(icmpSocket, destinationAddress, ID)
         
         # 2. Checksum ICMP packet using given function
-        checksum = NetworkApplication.checksum(NetworkApplication, packet)
+        checksum = NetworkApplication.checksum(NetworkApplication, pack)
         
         # 3. Insert checksum into packet
-        packet = self.buildHeader(checksum)
+        pack = self.buildHeader(icmpSocket, destinationAddress, ID, checksum)
         
         # 4. Send packet using socket
-        try:
-            icmpSocket.sendto(packet, (destinationAddress, 1)) #port doesn't matter for icmp message
-        except socket.error as msg:
-            print("Error sending icmp packet to destination. Error: " + str(msg))
-            sys.exit(1)
-            
-        # 5. Record time of sending
-        send_time = time.time()
-        return send_time
+        icmpSocket.sendto(pack, (destinationAddress, 0)) # port 0 for command/ICMP echo request
         
-    def doOnePing(self, destinationAddress, timeout):
+        # 5. Record time of sending
+        return time.time()
+
+    def doOnePing(self, destinationAddress, timeout=1, sequence_num=1):
         # 1. Create ICMP socket
         try:
-                icmpSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
+                sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
+                #sock.bind((socket.gethostname, 0))
         except socket.error as msg:
                 print("Socket could not be created. Error: " + str(msg))
                 sys.exit(1)
-        
+                
         # 2. Call sendOnePing function
-        send_time = self.sendOnePing(icmpSocket, destinationAddress, self.own_id)
+        send_time = self.sendOnePing(sock, destinationAddress, sequence_num)
         
         # 3. Call receiveOnePing function
-        recv_details = self.receiveOnePing(icmpSocket, destinationAddress, self.own_id, timeout)
+        recv_time, reply_size = self.receiveOnePing(sock, destinationAddress, sequence_num, timeout)
         
         # 4. Close ICMP socket
-        icmpSocket.close()
+        sock.close()
         
         # 5. Return total network delay
-        total_delay = recv_details[0] - send_time
-        ping_details = (total_delay, packet_len, ttl)
-
+        #print("send time = %f, recv time = %f" % (send_time, recv_time))
+        total_delay = recv_time - send_time
+        print("[TESTING] Total delay = %f" % (total_delay))
+        ping_details = (total_delay, reply_size)
+        
         return ping_details
 
     def __init__(self, args):
         print('Ping to: %s...' % (args.hostname))
-
-        self.own_id = os.getpid() & 0xFFFF
-        self.seq_number = 1
         
         # 1. Look up hostname, resolving it to an IP address
         try:
@@ -196,20 +203,27 @@ class ICMPPing(NetworkApplication):
                 print("gaierror has occurred: " + str(msg))
                 sys.exit(1)
 
+        if not self.check_valid_ip4_addr(ip):
+            print("That hostname doesn't resolve to a valid ip address")
+            sys.exit(1)
+        
         print("IP = " + ip)
         print("-"*32)
         
         # 2. Call doOnePing function, approximately every second
-        while True:
+        running = True
+        sequence_num = 1
+        while(running):
                 time.sleep(1)
-
-                ping_details = self.doOnePing(ip, 5)
+                
+                ping_details = self.doOnePing(ip, 5, sequence_num)
                 
                 # 3. Print out the returned delay (and other relevant details) using the printOneResult method
-                self.printOneResult(ip, ping_details[1], ping_details[0], ping_details[2]) #destAddr, packLen, delayTime, ttl, destHostName(optional)
+                # destinationAddress: str, packetLength: int, time: float, ttl: int, destinationHostname=''
+                self.printOneResult(ip, ping_details[1], ping_details[0], 150)
                 
                 # 4. Continue this process until stopped
-                self.seq_number += 1
+                sequence_num += 1
 
 
 class Traceroute(NetworkApplication):
